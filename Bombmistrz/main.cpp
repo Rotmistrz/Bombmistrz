7fe39b3e27c0f33e17ca7764bc96368e19de3b92
MrLukashem <mertalukasz@gmail.com>
2014-10-20 20:35:23 +0200

    VS + skonfigurowany sfml

---
+++ Bombmistrz/main.cpp
@@ -0,0 +1,23 @@
+#include "Header.h"
+
+int main(int, char**) {
+	sf::RenderWindow window(sf::VideoMode(400, 400), "Hello Grzesiek !");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
